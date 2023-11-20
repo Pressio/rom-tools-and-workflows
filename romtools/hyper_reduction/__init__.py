a6445fe9f1dfbc8ef80c2c6e6a41e296fa29461f
Patrick Blonigan <pblonig@sandia.gov>
2023-11-20 09:46:21 -0700

    Update __init__.py
    Remove leftover comments from previous merge, as suggested.

--- romtools/hyper_reduction/__init__.py
+++ romtools/hyper_reduction/__init__.py
@@ -1,5 +1,4 @@
 '''
-<<<<<<< HEAD
 The hyper-reduction library comprises a set of routines for hyper-reduction. Presently, we support algorithms for
 
 - The discrete empirical interpolation method (DEIM)
