ce0ead0110faf34f7f61354885dfe2ea4118af95
Blonigan <pblonig@s1100484.srn.sandia.gov>
2023-12-17 09:18:51 -0700

    Made variable names more expressive in ECSW routines

--- romtools/hyper_reduction/ecsw.py
+++ romtools/hyper_reduction/ecsw.py
@@ -60,128 +60,130 @@ class ECSWsolverNNLS(AbstractECSWsolver):
     computed via non-negative least squares algorithm from Chapman et al. 2016
     DOI: 10.1002/nme.5332.
 
-    G is a [n_snaps*n_rom by n_dofs] numpy ndarray, where n_snaps is the
-    number of residual snapshots, n_rom is the ROM dimension, and n_dofs is
+    full_mesh_lhs is a [n_snap*n_rom by n_dof] numpy ndarray, where n_snap is the
+    number of residual snapshots, n_rom is the ROM dimension, and n_dof is
     the number of mesh degrees of freedom (DoFs) (nodes, volumes, or elements)
 
-    b is a [n_snaps*n_rom] length numpy array
+    full_mesh_rhs is a [n_snap*n_rom] length numpy array
 
-    tau is the ECSW tolerance parameter. Lower values of tau will result in
+    tolerance is the ECSW tolerance parameter. Lower values of tolerance will result in
     more mesh DoF samples
     '''
 
     def __init__(self, solver_param_dict: dict = None):
         # Set default solver parameter values
-        self.k_max = 10000  # maximum overall iterations
-        self.j_max = 100  # maximum inner iterations to enforce non-negativity
+        self.max_iters = 10000  # maximum overall iterations
+        self.max_non_neg_iters = 100  # maximum inner iterations to enforce non-negativity
         self.max_iters_res_unchanged = 10  # maximum number of iterations without any change in residual before terminating
         self.zero_tol = 1e-12  # tolerance used to check if weights or residual norm changes are near zero
 
         if solver_param_dict is not None:
             if 'max_iters' in solver_param_dict.keys():
-                self.k_max = solver_param_dict['max_iters']
+                self.max_iters = solver_param_dict['max_iters']
             if 'max_non_neg_iters' in solver_param_dict.keys():
-                self.j_max = solver_param_dict['max_non_neg_iters']
+                self.max_non_neg_iters = solver_param_dict['max_non_neg_iters']
             if 'max_iters_res_unchanged' in solver_param_dict.keys():
                 self.max_iters_res_unchanged = solver_param_dict['max_iters_res_unchanged']
             if 'zero_tol' in solver_param_dict.keys():
                 self.zero_tol = solver_param_dict['zero_tol']
 
-    def __call__(self, G: np.ndarray, b: np.array, tau: np.double):
-        # min || G*xi-b||_2^2 s.t. xi >=0, || G*xi-b||_2 < tau ||b||_2
-        n_dofs = G.shape[1]
+    def __call__(self, full_mesh_lhs: np.ndarray, full_mesh_rhs: np.array, tolerance: np.double):
+        # Solve min || full_mesh_lhs*full_mesh_weights-full_mesh_rhs ||_2^2 s.t. full_mesh_weights >=0, || full_mesh_lhs*full_mesh_weights-full_mesh_rhs ||_2 < tolerance ||full_mesh_rhs||_2
+
+        n_dof = full_mesh_lhs.shape[1]
 
         # initialize
-        k = 0
-        I_k = []
-        r_k = b.copy()
-        r_k_norm = np.linalg.norm(r_k)
-        tau_b_norm = tau*r_k_norm
+        iters = 0
+        sample_mesh_indicies = []
+        residual = full_mesh_rhs.copy()
+        residual_norm = np.linalg.norm(residual)
+        target_norm = tolerance*residual_norm
 
-        xi_k = np.zeros(n_dofs)
-        zeta_k = np.zeros(n_dofs)
+        full_mesh_weights = np.zeros(n_dof)
+        full_mesh_candidate_weights = np.zeros(n_dof)
 
-        r_k_norm_unchanged = 0
+        residual_norm_unchanged = 0
 
         # add nodes to sample mesh until tolerance is met
-        while (r_k_norm > tau_b_norm) and (r_k_norm_unchanged < self.max_iters_res_unchanged) and (k < self.k_max):
+        while (residual_norm > target_norm) and (residual_norm_unchanged < self.max_iters_res_unchanged) and (iters < self.max_iters):
             # determine new node to add to sample mesh
-            mu_k = np.dot(G.T, r_k)
+            weighted_residual = np.dot(full_mesh_lhs.T, residual)
 
             # make sure mesh entity hasn't already been selected
             still_searching = True
             while still_searching:
-                i_k = np.argmax(mu_k)
+                mesh_index = np.argmax(weighted_residual)
                 still_searching = False
-                if i_k in I_k:
+                if mesh_index in sample_mesh_indicies:
                     still_searching = True
-                    mu_k[i_k] = np.min(mu_k)
+                    weighted_residual[mesh_index] = np.min(weighted_residual)
 
             # add new mesh entity index
-            I_k.append(i_k)
+            sample_mesh_indicies.append(mesh_index)
 
-            print("k={}  n_samps={}  ||r_k||={}  tau*||b||={} ".format(k, len(I_k), r_k_norm, tau_b_norm))
-            k += 1
+            print("iteration={}  sample mesh size={}  residual norm={:.8e}  ratio to target={:.8e} ".format(iters, len(sample_mesh_indicies), residual_norm, residual_norm/target_norm))
+            iters += 1
 
             # compute corresponding weights
 
-            for j in range(self.j_max):
-                G_I_k = G[:, I_k]
-                zeta_I_k = np.dot(np.linalg.pinv(G_I_k), b)
-                zeta_k *= 0
-                zeta_k[I_k] = zeta_I_k
-                if np.all(zeta_I_k > 0):
-                    xi_k = zeta_k.copy()
+            for j in range(self.max_non_neg_iters):
+                sample_mesh_lhs = full_mesh_lhs[:, sample_mesh_indicies]
+                sample_mesh_candidate_weights = np.dot(np.linalg.pinv(sample_mesh_lhs), full_mesh_rhs)
+                full_mesh_candidate_weights *= 0
+                full_mesh_candidate_weights[sample_mesh_indicies] = sample_mesh_candidate_weights
+                if np.all(sample_mesh_candidate_weights > 0):
+                    full_mesh_weights = full_mesh_candidate_weights.copy()
                     break
                 # line search to enforce non-negativity
-                max_step = self.__max_feasible_step(xi_k[I_k], zeta_I_k)
-                xi_kp1 = xi_k + max_step * (zeta_k - xi_k)
+                max_step = self.__max_feasible_step(full_mesh_weights[sample_mesh_indicies], sample_mesh_candidate_weights)
+                full_mesh_weights_new = full_mesh_weights + max_step * (full_mesh_candidate_weights - full_mesh_weights)
 
                 # remove zero valued indices
-                inds = np.nonzero(xi_kp1[I_k] < self.zero_tol)[0]
-                samp_inds_for_removal = [I_k[i] for i in inds]
+                near_zero_inds = np.nonzero(full_mesh_weights_new[sample_mesh_indicies] < self.zero_tol)[0]
+                samp_inds_for_removal = [sample_mesh_indicies[i] for i in near_zero_inds]
                 for samp_ind in samp_inds_for_removal:
-                    I_k.remove(samp_ind)
+                    sample_mesh_indicies.remove(samp_ind)
 
                 # increment iteration count
-                k += 1
-                xi_k = 1*xi_kp1
-                print("k={}  n_samps={}  ||r_k||={}  tau*||b||={} ".format(k, len(I_k), r_k_norm, tau_b_norm))
+                print("iteration={}  sample mesh size={}  residual norm={:.8e}  ratio to target={:.8e} ".format(iters, len(sample_mesh_indicies), residual_norm, residual_norm/target_norm))
+                iters += 1
+                full_mesh_weights = 1*full_mesh_weights_new
 
-            if j == self.j_max-1:
+
+            if j == self.max_non_neg_iters-1:
                 sys.exit("Error: NNLS algorithm failed to compute weights")
 
-            # update least-squares residual r_k
-            xi_I_k = xi_k[I_k]
-            r_k = b - np.dot(G_I_k, xi_I_k)
-            r_km1_norm = 1*r_k_norm
-            r_k_norm = np.linalg.norm(r_k)
+            # update least-squares residual
+            sample_mesh_weights = full_mesh_weights[sample_mesh_indicies]
+            residual = full_mesh_rhs - np.dot(sample_mesh_lhs, sample_mesh_weights)
+            residul_old_norm = 1*residual_norm
+            residual_norm = np.linalg.norm(residual)
 
-            if np.abs(r_k_norm - r_km1_norm) < self.zero_tol:
-                r_k_norm_unchanged += 1
+            if np.abs(residual_norm - residul_old_norm) < self.zero_tol:
+                residual_norm_unchanged += 1
             else:
-                r_k_norm_unchanged = 0
+                residual_norm_unchanged = 0
 
-            if (r_k_norm_unchanged >= self.max_iters_res_unchanged):
+            if (residual_norm_unchanged >= self.max_iters_res_unchanged):
                 print("WARNING: Norm has not changed more than {} in {} steps, exiting NNLS".format(self.zero_tol, self.max_iters_res_unchanged))
 
         print("NNLS complete! Final stats:")
-        print("k={}  n_samps={}  ||r_k||={}  tau*||b||={} ".format(k, len(I_k), r_k_norm, tau_b_norm))
+        print("iteration={}  sample mesh size={}  residual norm={:.8e}  ratio to target={:.8e} ".format(iters, len(sample_mesh_indicies), residual_norm, residual_norm/target_norm))
 
-        return I_k, xi_k
+        return sample_mesh_indicies, full_mesh_weights
 
-    def __max_feasible_step(self, xi, zeta):
+    def __max_feasible_step(self, weights, candidate_weights):
         '''
         determine maximum step size such that:
-        xi + step_size * (zeta-xi) >=0
+        weights + step_size * (candidate_weights-weights) >=0
         '''
-        inds = np.argwhere(zeta <= 0)
+        inds = np.argwhere(candidate_weights <= 0)
         step_size = 1.0
         for i in inds:
-            if (xi[i] == 0.0):
+            if (weights[i] == 0.0):
                 step_size = 0
             else:
-                step_size_i = xi[i] / (xi[i] - zeta[i])
+                step_size_i = weights[i] / (weights[i] - candidate_weights[i])
                 step_size = min([step_size, step_size_i])
         return step_size
 
@@ -190,7 +192,7 @@ class ECSWsolverNNLS(AbstractECSWsolver):
 
 def _construct_linear_system(residual_snapshots: np.ndarray,
                              test_basis: np.ndarray,
-                             n_vars: int,
+                             n_var: int,
                              variable_ordering: str):
     '''
     Make linear system for ECSW with a fixed test basis, such as POD-Galerkin
@@ -204,13 +206,13 @@ def _construct_linear_system(residual_snapshots: np.ndarray,
 
     Return the matrix and right-hand-side
 
-    residual_snapshots is a n_dofs*n_vars by n_snaps array, where n_dofs is
-    the number of mesh degrees of freedom, n_vars is the number of residual
+    residual_snapshots is a n_dof*n_var by n_snap array, where n_dof is
+    the number of mesh degrees of freedom, n_var is the number of residual
     variables (e.g. for fluid flow, residual variable could be mass,
-    x-momentum, y-momentum, z-momentum, and energy), and n_snaps is the number
+    x-momentum, y-momentum, z-momentum, and energy), and n_snap is the number
     of snapshots
 
-    test_basis is a n_dofs*n_vars by n_modes array, where n_modes is the
+    test_basis is a n_dof*n_var by n_mode array, where n_mode is the
     number of modes in the basis.
 
     variable_ordering is a character specifying the ordering of variables and
@@ -219,21 +221,21 @@ def _construct_linear_system(residual_snapshots: np.ndarray,
     F: mesh DoFs are the fastest index
     '''
 
-    (n_rows, n_snaps) = residual_snapshots.shape
-    n_dofs = int(n_rows / n_vars)
-    n_modes = test_basis.shape[1]
+    (n_row, n_snap) = residual_snapshots.shape
+    n_dof = int(n_row / n_var)
+    n_mode = test_basis.shape[1]
     # construct ECSW system
-    full_mesh_lhs = np.zeros((n_snaps*n_modes, n_dofs))
+    full_mesh_lhs = np.zeros((n_snap*n_mode, n_dof))
 
     # left-hand side
-    for i in range(n_dofs):
+    for i in range(n_dof):
         # should be projection of all variables for a given mesh DoF
         if variable_ordering == 'C':
-            Phi_block = test_basis[(i*n_vars):((i+1)*n_vars),:]  # n_vars x n_modes
-            resSnaps_block = residual_snapshots[(i*n_vars):((i+1)*n_vars), :]  # n_vars x n_snaps
+            Phi_block = test_basis[(i*n_var):((i+1)*n_var),:]  # n_var x n_mode
+            resSnaps_block = residual_snapshots[(i*n_var):((i+1)*n_var), :]  # n_var x n_snap
         elif variable_ordering == 'F':
-            Phi_block = test_basis[i::n_dofs, :]  # n_vars x n_modes
-            resSnaps_block = residual_snapshots[i::n_dofs, :]  # n_vars x n_snaps
+            Phi_block = test_basis[i::n_dof, :]  # n_var x n_mode
+            resSnaps_block = residual_snapshots[i::n_dof, :]  # n_var x n_snap
 
         full_mesh_lhs_block = np.dot(Phi_block.T, resSnaps_block)  # Nmodes x Nsnaps matrix
         full_mesh_lhs[:, i] = np.ravel(full_mesh_lhs_block, order='F')
@@ -247,9 +249,9 @@ def _construct_linear_system(residual_snapshots: np.ndarray,
 def ecsw_fixed_test_basis(ecsw_solver: AbstractECSWsolver,
                           residual_snapshots: np.ndarray,
                           test_basis: np.ndarray,
-                          n_vars: int,
+                          n_var: int,
                           variable_ordering: str,
-                          tau: np.double):
+                          tolerance: np.double):
     '''
     ECSW implementation for a fixed test basis, such as POD-Galerkin projection
 
@@ -261,13 +263,13 @@ def ecsw_fixed_test_basis(ecsw_solver: AbstractECSWsolver,
 
     Return mesh DoF weights and indices
 
-    residual_snapshots is a n_dofs*n_vars by n_snaps array, where n_dofs is
-    the number of mesh degrees of freedom, n_vars is the number of residual
+    residual_snapshots is a n_dof*n_var by n_snap array, where n_dof is
+    the number of mesh degrees of freedom, n_var is the number of residual
     variables (e.g. for fluid flow, residual variable could be mass,
-    x-momentum, y-momentum, z-momentum, and energy), and n_snaps is the number
+    x-momentum, y-momentum, z-momentum, and energy), and n_snap is the number
     of snapshots
 
-    test_basis is a n_dofs*n_vars by n_modes array, where n_modes is the
+    test_basis is a n_dof*n_var by n_mode array, where n_mode is the
     number of modes in the basis.
 
     variable_ordering is a character specifying the ordering of variables and
@@ -275,22 +277,22 @@ def ecsw_fixed_test_basis(ecsw_solver: AbstractECSWsolver,
     C: variables are the fastest index
     F: mesh DoFs are the fastest index
 
-    tau is the tolerance for the ecsw_solver. It is a small (<<1) positive
+    tolerance is the tolerance for the ecsw_solver. It is a small (<<1) positive
     number
     '''
 
     # TODO need to incorporate residual scales here too, perhaps using scaler.py
     full_mesh_lhs, full_mesh_rhs = _construct_linear_system(residual_snapshots,
                                                             test_basis,
-                                                            n_vars,
+                                                            n_var,
                                                             variable_ordering)
 
-    return ecsw_solver(full_mesh_lhs, full_mesh_rhs, tau)
+    return ecsw_solver(full_mesh_lhs, full_mesh_rhs, tolerance)
 
 def ecsw_varying_test_basis(ecsw_solver: AbstractECSWsolver,
                             full_mesh_lhs: np.ndarray,
                             full_mesh_rhs: np.ndarray,
-                            tau: np.double):
+                            tolerance: np.double):
     '''
     ECSW implementation for a fixed test basis, such as POD-Galerkin projection
 
@@ -308,9 +310,9 @@ def ecsw_varying_test_basis(ecsw_solver: AbstractECSWsolver,
 
     full_mesh_rhs is a Nsnaps*Nrom length numpy array
 
-    tau is the tolerance for the ecsw_solver. It is a small (<<1) positive
+    tolerance is the tolerance for the ecsw_solver. It is a small (<<1) positive
     number
     '''
 
-    return ecsw_solver(full_mesh_lhs, full_mesh_rhs, tau)
+    return ecsw_solver(full_mesh_lhs, full_mesh_rhs, tolerance)
 
