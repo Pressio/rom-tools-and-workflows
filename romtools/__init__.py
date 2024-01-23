af1af22398d0b9e913fb183cb116e0e867393a5b
Francesco Rizzi <fnrizzi@sandia.gov>
2024-01-23 15:23:06 +0100

    remove leftover stuff of snapshot class

--- romtools/__init__.py
+++ romtools/__init__.py
@@ -62,7 +62,6 @@ Once this class is complete, the user gains access to all of our existing algori
 The Python library, called `romtools`, contains abstract interfaces and functions required for, e.g.,
 
 - Constructing parameter spaces
-- Constructing a snapshot data class
 - Constructing trial spaces
 - Constructing and exploiting ROMs via outer loop workflows
 
@@ -80,10 +79,6 @@ The Python library, called `romtools`, contains abstract interfaces and function
 
 
 ## Representative abstract base classes
-
-- `AbstractSnapshotData`
-  - This class defines the minimum API requirements for a "snapshot_data" class that will be used in the construction
-    of a trial space.
 
 - `AbstractTrialSpace`
   - This class defines the minimum API requirements for a trial space
@@ -107,66 +102,8 @@ The Python library, called `romtools`, contains abstract interfaces and function
 
 In this section, we present some demos/examples showing how to use `romtools` in practice.
 
-## 1. Snapshot data
+TBD
 
-The first demo is a simple example of how you could use collect snapshot data for a basic problem.\
-For the sake of exposition, suppose that we are solving the [1D heat equation](https://aquaulb.github.io/book_solving_pde_mooc/solving_pde_mooc/notebooks/04_PartialDifferentialEquations/04_03_Diffusion_Explicit.html)
-
-$$\\partial_t T(x,t) = \\alpha \\frac{d^2 T} {dx^2}(x,t) + \\sigma (x,t)$$
-
-where $x$ is space, $t$ is time, $T$ is the temperature, $\\alpha$ is the diffusivity,
-and $\\sigma$ is the source term.
-Instead of the numerical solution, for now let's work with the analytical solution:
-
-$$T(x,t)=e^{-4\\pi^2\\alpha t}\\sin(2\\pi x) + \\frac{2}{\\pi^2\\alpha}(1-e^{-\\pi^2\\alpha t})\\sin(\\pi x)$$
-
-
-```python
-import numpy as np
-
-def exact_solution(x,t, alpha):
-    """
-    Returns the exact solution of the 1D heat equation with
-    heat source term sin(np.pi*x) and initial condition sin(2*np.pi*x)
-
-    Parameters
-    ----------
-    x : array of floats, grid points coordinates
-    t : float, time
-
-    Returns
-    -------
-    f : array of floats, exact solution
-    """
-    f = (np.exp(-4*np.pi**2*alpha*t) * np.sin(2*np.pi*x)
-      + 2.0*(1-np.exp(-np.pi**2*alpha*t)) * np.sin(np.pi*x)
-      / (np.pi**2*alpha))
-
-    return f
-
-import romtools as rt
-class HeatSnapshots(rt.AbstractSnapshotData):
-    def __init__(self, snapshots: list):
-        rt.AbstractSnapshotData.__init__(self, var_names=['T'])
-        self.snapshots = snapshots
-
-    def getMeshGids(self):
-        # this method is a noop for now but needs to be defined
-        # since it is an abstract method in the base class
-        pass
-
-    def getSnapshotsAsListOfArrays(self):
-        return self.snapshots
-
-if __name__=="__main__":
-    numPoints, numTimes = 21, 11
-    x     = np.linspace(0., 1., numPoints)
-    times = np.linspace(0., 5., numTimes)
-
-    alpha = 0.1
-    data = [exact_solution(x, t, alpha) for t in times]
-    snapshots = HeatSnapshots(data)
-```
 # License
 ```plaintext
 .. include:: ../LICENSE
