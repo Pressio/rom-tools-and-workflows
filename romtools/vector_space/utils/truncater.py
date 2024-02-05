8db5d0148cf288712608ce829f14e0c1bc396627
Krath <ekrath@s1067086ca.ca.sandia.gov>
2024-02-05 10:43:00 -0800

    Small improvements to truncater #96

--- romtools/vector_space/utils/truncater.py
+++ romtools/vector_space/utils/truncater.py
@@ -57,6 +57,7 @@ of basis vectors and the decay of the singular values
 
 import numpy as np
 from typing import Protocol
+import warnings
 
 
 class LeftSingularVectorTruncater(Protocol):
@@ -93,6 +94,11 @@ class BasisSizeTruncater:
         Args:
             basis_dimension (int): The desired dimension of the truncated basis.
         '''
+
+        # Check if basis dimension is less than or equal to zero
+        if basis_dimension <= 0:
+            raise ValueError('Given basis dimension is <= 0: ', basis_dimension)
+        
         self.__basis_dimension = basis_dimension
 
     def truncate(self, basis: np.ndarray, singular_values: np.ndarray) -> np.ndarray:
@@ -106,6 +112,11 @@ class BasisSizeTruncater:
         Returns:
             np.ndarray: The truncated basis matrix with the specified dimension.
         '''
+
+        # Check if basis dimension is larger than array and give error.
+        if self.__basis_dimension > np.shape(basis)[1]:
+            raise ValueError('Given basis dimension is greater than size of basis array: ', self.__basis_dimension, ' > ', np.shape(basis)[1])
+
         return basis[:, :self.__basis_dimension]
 
 
@@ -120,9 +131,9 @@ class EnergyBasedTruncater:
         Constructor for the EnergyTruncater class.
 
         Args:
-            threshold (float): The cumulative energy threshold.
+            threshold (float): The cumulative energy threshold. (Recommended: 0.99).
         '''
-        self.energy_threshold_ = threshold
+        self.__energy_threshold = threshold
 
     def truncate(self, basis: np.ndarray, singular_values: np.ndarray) -> np.ndarray:
         '''
@@ -136,5 +147,5 @@ class EnergyBasedTruncater:
             np.ndarray: The truncated basis matrix based on the energy threshold.
         '''
         energy = np.cumsum(singular_values**2)/np.sum(singular_values**2)
-        basis_dimension = np.argmax(energy > self.energy_threshold_) + 1
+        basis_dimension = np.argmax(energy > self.__energy_threshold) + 1
         return basis[:, 0:basis_dimension]
