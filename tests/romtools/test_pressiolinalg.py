3727611b59a4d0084b301ed3f408f2f662b97fc3
Caleb Schilly <cwschilly@gmail.com>
2024-02-12 16:13:51 -0500

    #70: improve test

--- tests/romtools/test_pressiolinalg.py
+++ tests/romtools/test_pressiolinalg.py
@@ -2,9 +2,15 @@ import pytest
 import numpy as np
 import romtools as rt
 import pressiolinalg.linalg as pla
+try:
+    import mpi4py
+    from mpi4py import MPI
+except ModuleNotFoundError:
+    print("module 'mpi4py' is not installed")
+
 
 def test_pressiolinalg_serial():
-    test_arr = np.array([1,2,3,4,5,6])
+    test_arr = np.random.rand(6)
 
     np_max = np.max(test_arr)
     pla_max = pla.max(test_arr)
@@ -17,13 +23,10 @@ def test_pressiolinalg_serial():
 # Functionality is tested in pressio-linalg; this is just checking that MPI works
 @pytest.mark.mpi(min_size=3)
 def test_pressiolinalg_parallel():
-    import mpi4py
-    from mpi4py import MPI
-
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
 
-    global_vector = np.arange(0,size*2)
+    global_vector = np.random.rand(size*2)
     local_vector = np.zeros(2)
     comm.Scatter(global_vector, local_vector, root=0)
 
@@ -31,6 +34,7 @@ def test_pressiolinalg_parallel():
     serial_min = np.min(global_vector)
 
     assert dist_min == serial_min
+
 
 if __name__ == "__main__":
     test_pressiolinalg_serial()
