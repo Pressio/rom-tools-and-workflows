419e3a25bc660054ed2bca667bdad1280b183375
Caleb Schilly <cwschilly@gmail.com>
2024-02-12 14:53:34 -0500

    #70: add parallel test for pressiolinalg

--- tests/romtools/test_pressiolinalg.py
+++ tests/romtools/test_pressiolinalg.py
@@ -4,7 +4,6 @@ import romtools as rt
 import pressiolinalg.linalg as pla
 
 def test_pressiolinalg_serial():
-
     test_arr = np.array([1,2,3,4,5,6])
 
     np_max = np.max(test_arr)
@@ -15,5 +14,24 @@ def test_pressiolinalg_serial():
     pla_min = pla.min(test_arr)
     assert(np_min == pla_min)
 
+# Functionality is tested in pressio-linalg; this is just checking that MPI works
+@pytest.mark.mpi(min_size=3)
+def test_pressiolinalg_parallel():
+    import mpi4py
+    from mpi4py import MPI
+
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+
+    global_vector = np.arange(0,size*2)
+    local_vector = np.zeros(2)
+    comm.Scatter(global_vector, local_vector, root=0)
+
+    dist_min = pla.min(local_vector, comm)
+    serial_min = np.min(global_vector)
+
+    assert dist_min == serial_min
+
 if __name__ == "__main__":
     test_pressiolinalg_serial()
+    test_pressiolinalg_parallel()
