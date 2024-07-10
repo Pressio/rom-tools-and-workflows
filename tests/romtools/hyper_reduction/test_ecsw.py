7cd8ac666e1e41d10d841a6b7dd6d3ac54cbe16e
Blonigan <pblonig@s1100484.srn.sandia.gov>
2024-07-10 14:34:36 -0600

    Added ECSW support for LSPG with near-zero residual vectors by using test basis columns instead of residual snapshots

--- tests/romtools/hyper_reduction/test_ecsw.py
+++ tests/romtools/hyper_reduction/test_ecsw.py
@@ -122,8 +122,48 @@ def test_full_ecsw_varying_test_basis():
         assert np.allclose(sample_mesh_approx, approx)
 
 
+@pytest.mark.mpi_skip
+def test_full_ecsw_lspg_zero_residual():
+    # test ECSW
+    n_var = 2
+    n_dof = 10
+    n_snap = 5
+    n_mode = 3
+    nnls = ecsw.ECSWsolverNNLS()
+    test_basis = np.zeros((n_snap,n_dof*n_var, n_mode))
+    for i in range(n_snap):
+        test_basis[i],_ = np.linalg.qr(np.random.normal(size=(n_dof*n_var, n_mode)))
+
+    # create corresponding tensors
+    test_basis_tensor = np.zeros((n_snap, n_var, n_dof, n_mode))
+    for i in range(n_snap):
+        test_basis_tensor[i] = _matrix_to_tensor(n_var,test_basis[i])
+
+    sample_mesh_indices, sample_mesh_weights = ecsw.ecsw_lspg_zero_residual(nnls, test_basis_tensor, n_var, 1e-4)
+    full_mesh_weights = np.zeros(test_basis_tensor.shape[2])
+    full_mesh_weights[sample_mesh_indices] = sample_mesh_weights
+
+
+    sample_mesh_test_basis_tensor = test_basis_tensor[:,:,sample_mesh_indices, :]
+    
+    for i in range(n_snap):
+        # Check that the full approximation of the residual snapshots is correct
+        exact = (test_basis[i].T)@test_basis[i]
+        approx = (test_basis[i].T)@(np.diag(full_mesh_weights.repeat(n_var))@test_basis[i])
+
+        assert np.allclose(exact, approx)
+
+        # Check that the sample mesh indices are correct
+        sample_mesh_test_basis =  _tensor_to_matrix(sample_mesh_test_basis_tensor[i])
+
+        sample_mesh_approx = (sample_mesh_test_basis.T)@(np.diag(sample_mesh_weights.repeat(n_var))@sample_mesh_test_basis)
+
+        assert np.allclose(sample_mesh_approx, approx)
+
+
 if __name__ == "__main__":
-    test_full_ecsw_fixed_test_basis()
-    test_full_ecsw_varying_test_basis()
     test_ecsw_nnls()
     test_ecsw_matrix()
+    test_full_ecsw_fixed_test_basis()
+    test_full_ecsw_varying_test_basis()
+    test_full_ecsw_lspg_zero_residual()
