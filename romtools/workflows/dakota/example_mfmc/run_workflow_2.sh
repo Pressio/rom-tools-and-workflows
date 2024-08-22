b9813ada30a08ff84c14d39edd117e3a9f1ac60e
rloekvh <rloekvh@gmail.com>
2024-08-22 10:41:29 -0400

    Add more realistic MF UQ example

---
+++ romtools/workflows/dakota/example_mfmc/run_workflow_2.sh
@@ -0,0 +1,13 @@
+
+python3 /home/rloekvh/pressio-demoapps/meshing_scripts/create_full_mesh_for.py \
+        --problem diffreac2d -n 128 128 --outDir mesh_HF
+python3 /home/rloekvh/pressio-demoapps/meshing_scripts/create_full_mesh_for.py \
+        --problem diffreac2d -n 32 32 --outDir mesh_LF1
+python3 /home/rloekvh/pressio-demoapps/meshing_scripts/create_full_mesh_for.py \
+        --problem diffreac2d -n 16 16 --outDir mesh_LF2
+        
+python3 save_model_LF1.py
+python3 save_model_LF2.py
+python3 save_model_HF.py
+
+dakota -i dakota_mfmc.in -o dakota.out
---
+++ romtools/workflows/dakota/example_mfmc/save_model_HF.py
@@ -0,0 +1,48 @@
+import sys
+import subprocess
+import os
+import numpy as np
+import yaml
+import romtools as rt
+from romtools.workflows.models import QoiModel
+import pickle
+import pathlib
+
+path_to_file = str(pathlib.Path(__file__).parent.resolve())
+
+class PDA_Model_HF(rt.workflows.models.QoiModel):
+    def __init__(self):
+        print("Initializing HF")
+        self.path = str(pathlib.Path(__file__).parent.resolve())
+        self.template_directory = self.path + '/templates'
+        self.template_file = "parameters.yaml"
+        return None
+
+    def populate_run_directory(self, run_directory, parameter_sample):
+        # Grab template yaml
+        with open(self.template_directory+"/"+self.template_file) as f:
+            input_yaml = yaml.safe_load(f)
+        
+        input_yaml['D'] = float(parameter_sample['D'])
+        input_yaml['k'] = float(parameter_sample['k'])
+
+        # Write yaml into run_directory 
+        with open(run_directory + "/" + self.template_file,"w") as f:
+            yaml.dump(input_yaml,f,default_flow_style=False)
+
+        return 0
+
+    def run_model(self, run_directory, parameter_sample):
+        print("Running model in directory " + str(run_directory))
+        os.system('python ' + run_directory + '/../../model.py')
+        return 0 # NOTE: This function must return 0
+
+    def compute_qoi(self, run_directory, parameter_sample):
+        data = np.load('solution.npz')
+        return np.array(np.amax(data['u']))
+
+if __name__ == "__main__":
+    # Define FOM
+    fom_model = PDA_Model_HF()
+    with open('HF.pickle', 'wb') as handle:
+        pickle.dump(fom_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
