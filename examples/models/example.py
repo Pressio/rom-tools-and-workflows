5a4116a6fb7ce3ebcc4809b33124db0f670cd786
eparish1 <eric.parish9@gmail.com>
2026-09-10 20:13:25 -0500

    List H2-air flame example model

--- examples/models/example.py
+++ examples/models/example.py
@@ -1,2 +1,3 @@
 print('This example is notebook-based.\nOpen: docs/source/demos/notebooks/basic_model.ipynb\n      docs/source/demos/notebooks/external_model.ipynb\n      docs/source/demos/notebooks/external_qoi_model.ipynb\n')
 print('Steady convection-diffusion-reaction model:\n      python examples/models/steady_cdr_model.py\n')
+print('H2-air flame benchmark model:\n      python examples/models/h2_air_flame_model.py\n')
