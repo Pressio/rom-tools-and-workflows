4c1acd93446ba147032ae0237476e3b22cbf860e
eparish1 <eric.parish9@gmail.com>
2026-09-13 13:48:31 -0500

    Avoid wildcard leakage from inverse workflows

--- romtools/workflows/inverse/__init__.py
+++ romtools/workflows/inverse/__init__.py
@@ -21,13 +21,20 @@ Optimization helpers and implementation utilities remain available from their
 defining modules but are not part of the public compatibility contract.
 """
 
-from romtools.workflows.inverse.eki_drivers import *
-from romtools.workflows.inverse.mf_eki_drivers import *
-from romtools.workflows.inverse.mf_vi_drivers import *
-from romtools.workflows.inverse.vi_drivers import *
-from romtools.workflows.inverse.vi_optimization_methods import *
-from romtools.workflows.inverse.ego_drivers import *
-from romtools.workflows.inverse.ego_optimization_methods import *
+from . import (
+    ego_drivers,
+    ego_optimization_methods,
+    eki_drivers,
+    mf_eki_drivers,
+    mf_vi_drivers,
+    vi_drivers,
+    vi_optimization_methods,
+)
+from .ego_drivers import run_batch_ego, run_ego
+from .eki_drivers import run_eki
+from .mf_eki_drivers import mf_eki_with_auto_rom, run_mf_eki
+from .mf_vi_drivers import mf_vi_with_auto_rom, run_mf_vi
+from .vi_drivers import run_vi
 
 __all__ = [
     "run_eki",
