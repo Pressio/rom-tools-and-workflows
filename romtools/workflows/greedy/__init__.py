ad54dc78cd49d812fb15162c2c787ef96af3c4d6
eparish1 <eric.parish9@gmail.com>
2026-09-13 13:45:31 -0500

    Use explicit greedy workflow exports

--- romtools/workflows/greedy/__init__.py
+++ romtools/workflows/greedy/__init__.py
@@ -44,11 +44,9 @@
 #
 '''Implementation of the greedy workflow.'''
 
-from romtools.workflows.greedy import run_greedy as run_greedy_module
 from romtools.workflows.greedy.run_greedy import QoIvsErrorIndicatorRegressor, run_greedy
 
 __all__ = [
-    "run_greedy_module",
     "run_greedy",
     "QoIvsErrorIndicatorRegressor",
 ]
