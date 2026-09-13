9b06a7109c25de70b0e1f75759c1dcbecd4a93f7
eparish1 <eric.parish9@gmail.com>
2026-09-13 08:05:36 -0500

    Standardize UQ work directory arguments

--- romtools/workflows/uq/__init__.py
+++ romtools/workflows/uq/__init__.py
@@ -1,10 +1,18 @@
 """Monte Carlo uncertainty-quantification workflows."""
 
-from romtools.workflows.uq.monte_carlo import (
-    MonteCarloResult,
-    MultifidelityMonteCarloResult,
-    run_monte_carlo,
-    run_multifidelity_monte_carlo,
+from importlib import import_module
+
+from romtools.workflows._work_dir_compat import patch_work_dir_argument
+
+_monte_carlo = import_module("romtools.workflows.uq.monte_carlo")
+
+MonteCarloResult = _monte_carlo.MonteCarloResult
+MultifidelityMonteCarloResult = _monte_carlo.MultifidelityMonteCarloResult
+run_monte_carlo = patch_work_dir_argument(
+    _monte_carlo, "run_monte_carlo", "absolute_uq_directory"
+)
+run_multifidelity_monte_carlo = patch_work_dir_argument(
+    _monte_carlo, "run_multifidelity_monte_carlo", "absolute_uq_directory"
 )
 
 __all__ = [
