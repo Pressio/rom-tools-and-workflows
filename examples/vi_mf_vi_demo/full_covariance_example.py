47b6809f488adf83ad1182ec63ac936b469336c0
eparish1 <eric.parish9@gmail.com>
2026-09-15 16:20:21 -0500

    Use unbounded Gaussian handling in full-covariance sine example

--- examples/vi_mf_vi_demo/full_covariance_example.py
+++ examples/vi_mf_vi_demo/full_covariance_example.py
@@ -85,6 +85,7 @@ def main(
         optimizer_config=optimizer,
         baseline_method="loo",
         score_function_entropy_strategy="joint",
+        bounded_parameter_handling="clip",
         random_seed=19,
         restart_files_to_keep=max_iterations + 1,
     )
